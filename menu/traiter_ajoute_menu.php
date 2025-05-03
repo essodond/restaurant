3db0b0d899c53b0407a51572a347779fd1405015
foster KAZALO <kazalowilliam@gmail.com>
2025-05-03 14:24:05 +0200

    finalation de la partie menu affichage et ajout des menu 2

--- menu/traiter_ajoute_menu.php
+++ menu/traiter_ajoute_menu.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // ... existing code ...
 
     if ($image && $image['error'] === UPLOAD_ERR_OK) {
-        
+        //kazalo
         
         // Keep original filename and extension
         $nomFichier = basename($image['name']);
